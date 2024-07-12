005e238cc2f3aa834698a8f0e8b2d0c9d89de258
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2024-07-12 15:00:17 -0600

    start with grammar ebnf

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,20 +1,23 @@
+#[derive(Debug)]
 pub enum Token {
+    // bad times
     Illegal(String),
+
+    // identifiers & literals
     Ident(String),
     Literal(String),
 
+    // normal keywords
     Let,
+    If,
+    Else,
+
+    // operator keywords
     Be,
     Do,
     End,
-}
-
-impl std::fmt::Display for Token {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            _ => write!(f, "Nothing here"),
-        }
-    }
+    Into,
+    With,
 }
 
 pub struct Scanner {
